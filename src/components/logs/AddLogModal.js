3657bf5c2633ac91b1d18e97767feb9966f6a913
akshaysl-ace <akshaysl3867@gmail.com>
2022-09-29 19:14:54 +0530

    added log actions, reducer

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { connect } from 'react-redux';
+import { addLog } from './../../actions/logActions';
+import { PropTypes } from 'prop-types';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
     const [message, setMessage] = useState("");
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState("");
@@ -10,7 +13,9 @@ const AddLogModal = () => {
         if (message === "" || tech === "") {
             M.toast({ html: "Please enter a message and a technician" });
         } else {
-            console.log("clicked");
+            const newLog = { message, tech, attention, date: new Date() };
+            addLog(newLog);
+            M.toast({ html: `Log added by ${tech}` });
             setMessage("");
             setTech("");
             setAttention(false);
@@ -54,9 +59,13 @@ const AddLogModal = () => {
     );
 }
 
+AddLogModal.propTypes = {
+    addLog: PropTypes.func.isRequired
+}
+
 const modalStyle = {
     width: "75%",
     height: "75%"
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { updateLog } from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
     const [message, setMessage] = useState("");
     const [attention, setAttention] = useState(false);
     const [tech, setTech] = useState("");
+
+    useEffect(() => {
+        if (current) {
+            setMessage(current.message);
+            setAttention(current.attention);
+            setTech(current.tech);
+        }
+    }, [current]);
 
     const onSubmit = () => {
         if (message === "" || tech === "") {
             M.toast({ html: "Please enter a message and a technician" });
         } else {
-            console.log("clicked");
+            const updatedLog = {
+                id: current.id,
+                message, attention, tech,
+                date: new Date()
+            }
+            updateLog(updatedLog);
+            M.toast({ html: `Log updated by ${tech}` });
             setMessage("");
             setTech("");
             setAttention(false);
@@ -55,9 +72,18 @@ const EditLogModal = () => {
     );
 }
 
+EditLogModal.propTypes = {
+    updateLog: PropTypes.func.isRequired,
+    current: PropTypes.object,
+}
+
+const mapStateToProps = state => ({
+    current: state.log.current
+})
+
 const modalStyle = {
     width: "75%",
     height: "75%"
 };
 
-export default EditLogModal;
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
